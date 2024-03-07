556053ba90ace7602e480690a98b9f11e0d90b08
Sealexey <tany--96@mail.re>
2024-03-08 00:20:13 +0300

    svg animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Burger} from "./UI/Burger/Burger";
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const [isActive, setIsActive] = useState(false)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <header>
+            <div onClick={() => setIsActive(!isActive)} >
+                <Burger width={40} isActive={isActive}/>
+            </div>
+
+        </header>
+        <svg className={`svg1 ${isActive ? 'active' : ''}`} xmlns="http://www.w3.org/2000/svg" width={100} height={100} viewBox="0 0 10 10">
+            <path d="M4,1 Q7,1 7,4 M7,4 Q7,7 4,7 M4,7 Q1,7 1,4 M1,4 Q1,1 4,1"/>
+        </svg>
     </>
   )
 }
